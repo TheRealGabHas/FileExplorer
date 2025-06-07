f9c9d705ce43b0340ad2d333e70e34be8b346fbc
TheRealGabHas <69684024+TheRealGabHas@users.noreply.github.com>
2025-06-07 21:17:01 +0200

    fix: flipped inequality

--- explorer.rb
+++ explorer.rb
@@ -50,7 +50,7 @@ class Explorer
     if File.exist?(next_path) && File.ftype(next_path) == "directory"
       if @configuration[:keep_history]
         @history << @current_path  # Store the last path
-        if @history.length > @configuration[:history_retention]
+        if @configuration[:history_retention] < @history.length
           @history = @history[1..]
         end
       end
