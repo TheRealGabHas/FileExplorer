6b020103edf98cb02cc0ecac90a31c65940f85d3
TheRealGabHas <69684024+TheRealGabHas@users.noreply.github.com>
2025-06-07 15:33:52 +0200

    fix(explorer): The file list is now scrollable. Longest file names are now cropped at the 47th char

--- main.rb
+++ main.rb
@@ -35,20 +35,35 @@ window.signal_connect("destroy") { Gtk.main_quit }
 def update_file_list(ex, container)
   # Remove the file list
   container.children.each do |child|
-    if child.is_a?(Gtk::Grid)  # Avoid removing the search bar
+    unless child.is_a?(Gtk::Entry)  # Avoid removing the search bar
       container.remove(child)
     end
   end
 
+  # The scrollable window that will contain the list of files
+  scroll_view = Gtk::ScrolledWindow.new
+  scroll_view.set_size_request(APP_DEFAULT_WIDTH, APP_DEFAULT_HEIGHT)
+  scroll_view.set_policy(Gtk::PolicyType::AUTOMATIC, Gtk::PolicyType::AUTOMATIC)
+
+  file_box = Gtk::Box.new(:vertical)
   # Listing the files/ directories
+  max_allowed_len = 40
   ex.listdir(show_hidden: true).each do |entry|
+    # Crop the longest names
+    if entry[:filename].length > max_allowed_len
+      entry[:filename] = "#{entry[:filename][0..(max_allowed_len - 3)]}..."
+    end
+
     grid = Gtk::Grid.new
     grid.set_column_homogeneous(true)
     grid.attach(Gtk::Label.new("\t#{entry[:filename]}").set_xalign(0.0), 0, 0, 1, 1)
     grid.attach(Gtk::Label.new("\t#{entry[:size]} o").set_xalign(0.0), 1, 0, 1, 1)
     grid.attach(Gtk::Label.new("\t#{entry[:type]}").set_xalign(0.0), 2, 0, 1, 1)
-    container.add(grid)
+    file_box.pack_start(grid, expand: false, fill: false, padding: 2)
   end
+
+  scroll_view.add(file_box)
+  container.add(scroll_view)
   container.show_all
 end
 
@@ -58,12 +73,12 @@ main_box = Gtk::Box.new(:vertical, 5)
 current_path_entry = Gtk::Entry.new.set_text(explorer.current_path)
 current_path_entry.signal_connect("key-press-event") do |widget, event|
   if event.keyval == Gdk::Keyval::KEY_Return
-    explorer.chdir(next_path: current_path_entry.text)
-    update_file_list(explorer, main_box)
+    explorer.chdir(next_path: current_path_entry.text)  # Set the new path
+    update_file_list(explorer, main_box)  # Update the displayed elements to match the current directory
   end
 end
 
-main_box.add(current_path_entry)
+main_box.pack_start(current_path_entry, expand: false, fill: false, padding: 5)
 update_file_list(explorer, main_box)
 
 window.add(main_box)
