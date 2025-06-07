390a177ecef19414fa93db03f6cdc315ca00bd1a
TheRealGabHas <69684024+TheRealGabHas@users.noreply.github.com>
2025-06-07 18:21:22 +0200

    feat(setting): Added history retention, started implementing a size formating setting

--- explorer.rb
+++ explorer.rb
@@ -11,25 +11,34 @@ class Explorer
     @history = []
     @configuration = {
       :show_hidden => false,  # Toggle the display of hidden files (starting with a dot)
+      :keep_history => true,  # Toggle memorization of the last viewed folders
+      :history_retention => 10,  # Number of previous visited folder that should be kept
+      :format_filesize => true,  # More human-readable file size (i.e. 4096 bytes -> 4 kilobytes)
     }
   end
 
   def listdir
     entries = []
     Dir.entries(@current_path).sort!.each do |entry|
+      # Ignore the file that starts with a dot if the settings is disabled
+      if (@configuration[:show_hidden] == false) && entry.start_with?(".")
+        next
+      end
+
       full_file_path = "#{@current_path}/#{entry}"
       if File.exist?(full_file_path)
+
+        size = File.size(full_file_path)
+        if @configuration[:format_filesize]
+          size = format_size(size)
+        end
+
         entries << {
           :filename => entry,
-          :size => File.size(full_file_path),  # file size in byte
+          :size => size,  # file size in byte
           :type => File.ftype(full_file_path)  # directory of file (mainly)
         }
       end
-    end
-
-    # Remove the file that starts with a dot
-    unless @configuration[:show_hidden]
-      entries.filter! {|entry| !entry[:filename].start_with?(".")}
     end
 
     entries
@@ -37,9 +46,19 @@ class Explorer
 
   def chdir(next_path:)
     if File.exist?(next_path)
-      @history << @current_path  # Store the last path
+      if @configuration[:keep_history]
+        @history << @current_path  # Store the last path
+        if @history.length > @configuration[:history_retention]
+          @history = @history[1..]
+        end
+      end
+
       @current_path = next_path
       @current_path = next_path[0..-2] if @current_path.end_with?("/")  # Trim the trailing slash if there is one
     end
+  end
+
+  def format_size(number)
+    number
   end
 end
